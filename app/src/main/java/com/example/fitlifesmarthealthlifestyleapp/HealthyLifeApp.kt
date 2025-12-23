010476af0e46c01be7b8d3784772cb71dbd816c2
23120353 <tranguyenquangthang2810@gmail.com>
2025-12-23 13:12:40 +0700

    initial commit

---
+++ app/src/main/java/com/example/fitlifesmarthealthlifestyleapp/HealthyLifeApp.kt
@@ -0,0 +1,4 @@
+package com.example.fitlifesmarthealthlifestyleapp
+
+class HealthyLifeApp {
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "FitLife – Smart Health & Lifestyle App"
+include(":app")
