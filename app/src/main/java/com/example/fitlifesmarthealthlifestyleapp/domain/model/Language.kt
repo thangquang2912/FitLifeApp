3f184ff8fa08886392e169bad65c55f9289d93ed
23120396 <quangtuanxml@gmail.com>
2026-01-28 18:24:30 +0700

    add: neccessary files for language

---
+++ app/src/main/java/com/example/fitlifesmarthealthlifestyleapp/domain/model/Language.kt
@@ -0,0 +1,6 @@
+package com.example.fitlifesmarthealthlifestyleapp.domain.model
+
+enum class Language(val code: String, val displayName: String) {
+    ENGLISH("en", "English"),
+    VIETNAMESE("vi", "Tiếng Việt")
+}
